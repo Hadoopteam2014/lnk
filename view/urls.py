60be271bc0b0271fae356b417395ebcd45084618
Anil <anil@wisblr1858a.dev.wissen.blr>
2014-07-01 15:46:03 +0530

    Testing commit

---
+++ view/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',url(r'^lkdn/$', 'viewer.views.index')
+    # Examples:
+    # url(r'^$', 'view.views.home', name='home'),
+    # url(r'^view/', include('view.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
