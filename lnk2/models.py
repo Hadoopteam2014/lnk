fa5e02efec3c3e0e25c1cb0a62413d60ad8b9e7e
Anil <anil@wisblr1858a.dev.wissen.blr>
2014-08-25 17:26:20 +0530

    fresh

--- lnk2/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.contrib.auth.models import User
-class UserProfile2(models.Model):
-    user = models.ForeignKey(User)
-    firstname = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
-    cid = models.CharField(max_length=300)
